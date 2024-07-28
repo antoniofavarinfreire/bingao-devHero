cc108c771983d48249fc40248be1960d28cd8969
Antonio Favarin Freire <42979335+antoniofavarinfreire@users.noreply.github.com>
2024-07-28 06:26:38 -0300

    novo deploy

--- vite.config.ts
+++ vite.config.ts
@@ -9,25 +9,22 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      injectRegister: 'auto',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'Bingo Mania',
         short_name: 'Bingo Mania',
         description: 'Jogo de Bingo com tabelas para o jogador e a máquina',
         theme_color: '#ffffff',
-        display: "standalone",
-        start_url: ".",
         icons: [
           {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            "src": "/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
           },
           {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+              "src": "/android-chrome-512x512.png",
+              "sizes": "512x512",
+              "type": "image/png"
           }
         ]
       }
